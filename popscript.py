83135da992ed3a362d9a62c38692b2f502dc4ece
NessMC <contact@nessmc.fr>
2020-05-06 17:00:37 +0200

    🔨 Comment parsing added.

--- popscript.py
+++ popscript.py
@@ -3,6 +3,10 @@
 # - Version: 1.0.0
 
 import re
+
+tokens = {
+  'COMMENT_INLINE': '--'
+}
 class PS:
   
   def __init__ (self):
@@ -11,6 +15,7 @@ class PS:
     self.posY     = 1
     self.cur_char = ''
     self.status   = 'S_FREE'
+    self.future_free = False
 
   def parse (self):
 
@@ -19,21 +24,36 @@ class PS:
     if file[0] == 'POPSCRIPT':
       for line in file:
         line_split = re.split('', line)[1:len(re.split('', line)) - 1]
+        comment = []
         if len(line_split) == 0:
           self.posX = -1
           self.posY += 1
           self.cur_char = ''
         else:
           for char in line_split:
-            if len(line_split) != 0:
-              self.posX += 1
-              self.cur_char = line_split[self.posX]
-              print(self.posX + 1, len(line_split), self.cur_char, self.posY)
-              if self.posX + 1 == len(line_split):
-                self.posX = -1
-                self.posY += 1
-                self.cur_char = ''
-              
+            self.posX += 1
+            self.cur_char = line_split[self.posX]
+            if self.future_free:
+              self.status = 'S_FREE'
+              self.future_free = False
+            if self.posX + 1 == len(line_split):
+              self.posX = -1
+              self.posY += 1
+              self.cur_char = ''
+              if self.status == 'S_COMMENT_INLINE':
+                self.future_free = True
+            
+            # Comments parsing
+            if self.status == 'S_COMMENT_INLINE':
+              comment.append(char)
+            if line_split[self.posX] == tokens['COMMENT_INLINE'][0]:
+              if line_split[self.posX + 1] == tokens['COMMENT_INLINE'][1]:
+                self.status = 'S_COMMENT_INLINE'
+                comment.append(char)
+
+            # print(verify)
+            print(char, self.status)
+        print('================================================') 
 
 popscript = PS()
 
