acaa513edaf03598c3adb3a955461b0256765711
NessMC <contact@nessmc.fr>
2020-06-03 11:54:14 +0200

    🔨 Basic tokens added.

--- core/tokens/tokens.js
+++ core/tokens/tokens.js
@@ -2,3 +2,16 @@
          POPSCRIPT LANGUAGE
                Tokens
 //////////////////////////////////*/
+
+module.exports = {
+
+  // BASIC
+
+  SPACE       : /\s/,
+  PROPERTY    : /:\w+/,
+  TABS        : /^\s+/,
+  COLON       : /:/,
+  DOT         : /\./,
+  COMMA       : /,/,
+
+}
