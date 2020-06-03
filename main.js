9a5e09b8560f2036b5b407092d32eb890b88e2b6
NessMC <contact@nessmc.fr>
2020-06-03 12:52:43 +0200

    🔨 Main file lexer module added.

--- main.js
+++ main.js
@@ -2,3 +2,18 @@
          POPSCRIPT LANGUAGE
                 Main
 //////////////////////////////////*/
+
+const lexer = require('./core/lexr'),
+      fs    = require('fs')
+
+fs.readFile('tests/even.ps', 'UTF-8', (error, content) => {
+    if (error) throw error
+       
+    content = content.split(/\r?\n/).filter(x => x !== '')
+
+    for (const index in content) {
+        const line = content[index]
+        console.log(new lexer(line).tokenize())
+    }
+
+})
