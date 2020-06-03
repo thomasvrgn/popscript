6cc224b6d06cb4d2bfb0fe858e2a9cc6428e2331
NessMC <contact@nessmc.fr>
2020-06-03 16:04:57 +0200

    🔨 Basic transpile class added.

--- core/transpiler.js
+++ core/transpiler.js
@@ -2,3 +2,36 @@
          POPSCRIPT LANGUAGE
              Transpiler
 //////////////////////////////////*/
+
+const lexer = require('./lexr') // Importing Lexer class
+
+module.exports = class {
+
+    constructor (content = '') {
+        
+        this.content = content.split(/\r?\n/g).filter(x => x !== '') // Splitting content by new line
+
+    }
+
+    transpile () {
+
+        for (const index in this.content) {
+
+            const line    = this.content[index],       // Defining line content with index variable
+                  lexered = new lexer(line).tokenize() // Lexering class call with line content
+
+            for (const lexer_item in lexered) {
+                
+                const item  = lexered[lexer_item] , // Defining lexered item by index
+                      token = item.token          , // Token item
+                      value = item.value            // Value item
+
+                console.log(token, value)
+
+            }
+
+        }
+
+    }
+
+}
--- main.js
+++ main.js
@@ -3,17 +3,12 @@
                 Main
 //////////////////////////////////*/
 
-const lexer = require('./core/lexr'),
-      fs    = require('fs')
+const transpiler = require('./core/transpiler'),
+      fs         = require('fs')
 
 fs.readFile('tests/even.ps', 'UTF-8', (error, content) => {
     if (error) throw error
        
-    content = content.split(/\r?\n/).filter(x => x !== '')
-
-    for (const index in content) {
-        const line = content[index]
-        console.log(new lexer(line).tokenize())
-    }
+    new transpiler(content).transpile()
 
 })
