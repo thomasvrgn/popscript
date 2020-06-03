bd1c0f203748da7030c99fc31dc3b197d8eb2359
NessMC <contact@nessmc.fr>
2020-06-03 16:19:33 +0200

    💬 Comments transpiler variables added.

--- core/transpiler.js
+++ core/transpiler.js
@@ -17,14 +17,18 @@ module.exports = class {
 
         for (const index in this.content) {
 
-            const line    = this.content[index],       // Defining line content with index variable
-                  lexered = new lexer(line).tokenize() // Lexering class call with line content
-
+            const line      = this.content[index]        , // Defining line content with index variable
+                  lexered   = new lexer(line).tokenize() , // Lexering class call with line content
+                  built     = []                           // Variable will contain line content
+            let   context   = undefined                  , // Line context. e.g.: function
+                  condition = false                      , // Condition line status
+                  variable  = false                      , // Variable line status    
+                  type_ctx  = undefined                    // Variable type context
             for (const lexer_item in lexered) {
                 
-                const item  = lexered[lexer_item] , // Defining lexered item by index
-                      token = item.token          , // Token item
-                      value = item.value            // Value item
+                const item  = lexered[lexer_item]        , // Defining lexered item by index
+                      token = item.token                 , // Token item
+                      value = item.value                   // Value item
 
                 console.log(token, value)
 
