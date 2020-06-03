675986437ee392973bff18b2040f3a42d95f70fe
NessMC <contact@nessmc.fr>
2020-06-03 12:52:21 +0200

    🔨 Lexer class added.

--- core/lexr.js
+++ core/lexr.js
@@ -2,3 +2,26 @@
          POPSCRIPT LANGUAGE
                 Lexr
 //////////////////////////////////*/
+
+const tokens = require('./tokens/tokens'),
+      lexr   = require('lexr')
+
+module.exports = class {
+
+    constructor (content) {
+
+        this.content = content
+
+    }
+
+    tokenize () {
+
+        const lexer = new lexr.Tokenizer('')
+
+        lexer.addTokenSet(tokens)
+
+        return lexer.tokenize(this.content)
+
+    }
+
+}
