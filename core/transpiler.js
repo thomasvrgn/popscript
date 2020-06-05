63e9d6f62f42ad3481d997cb9bf094f582a26d61
NessMC <contact@nessmc.fr>
2020-06-05 10:48:36 +0200

    💬 Transpiler comments updated.

--- core/transpiler.js
+++ core/transpiler.js
@@ -3,14 +3,14 @@
              Transpiler
 //////////////////////////////////*/
 
-const lexer = require('./lexr')                            // Importing Lexer class
+const lexer = require('./lexr')                             // Importing Lexer class
 
 module.exports = class {
 
     constructor (content = '') {
         
         this.content = content.split(/\r?\n/g)
-                              .filter(x => x !== '')       // Splitting content by new line
+                              .filter(x => x !== '')        // Splitting content by new line
 
     }
 
@@ -20,55 +20,57 @@ module.exports = class {
         const func_args     = {}
         let   func          = undefined
         for (const index in this.content) {
-            const line      = this.content[index]        , // Defining line content with index variable
-                  lexered   = new lexer(line).tokenize() , // Lexering class call with line content
-                  built     = []                           // Variable will contain line content
-            let   context   = undefined                  , // Line context. e.g.: function
-                  condition = false                      , // Condition line status
-                  variable  = false                      , // Variable line status    
-                  type_ctx  = undefined                    // Variable type context
+            const line      = this.content[index]        ,  // Defining line content with index variable
+                  lexered   = new lexer(line).tokenize() ,  // Lexering class call with line content
+                  built     = []                            // Variable will contain line content
+            let   context   = undefined                  ,  // Line context. e.g.: function
+                  condition = false                      ,  // Condition line status
+                  variable  = false                      ,  // Variable line status    
+                  type_ctx  = undefined                     // Variable type context
             for (const lexer_item in lexered) {
                 
-                const item  = lexered[lexer_item]        , // Defining lexered item by index
-                      token = item.token                 , // Token item
-                      value = item.value                   // Value item
+                const item  = lexered[lexer_item]        ,  // Defining lexered item by index
+                      token = item.token                 ,  // Token item
+                      value = item.value                    // Value item
 
                 switch (token) {
 
-                    case 'FUNCTION': {                     // Refer to function keyword (fn)
-                        context = token                    // Setting context to function
+                    case 'FUNCTION': {                      // Refer to function keyword (fn)
+                        context = token                     // Setting context to function
                         built.push('function')
                         break
                     }
 
-                    case 'ARGUMENTS': {                    // Refer to function arguments (=>)
-                        context = token                    // Setting context to arguments
+                    case 'ARGUMENTS': {                     // Refer to function arguments (=>)
+                        context = token                     // Setting context to arguments
                         built.push('(')
                         break
                     }
 
-                    case 'WORD': {                         // Refer to words
+                    case 'WORD': {                          // Refer to words
                         if (context === 'FUNCTION') {
-                            functions.push(value)          // Pushing function name to function list
+                            functions.push(value)           // Pushing function name to function list
                             built.push(value)
                             func_args[value] = []
                             func = value
                         } else if (context === 'ARGUMENTS') {
-                            built.push(value)              // Pushing function argument to built line
-                            if (func_args[func]) func_args[func].push(value)
+                            built.push(value)               // Pushing function argument to built line
+                            if (func_args[func]) {
+                                func_args[func].push(value) // Pushing function argument to function array
+                            }
                         } else {
                             if (functions.includes(value)) {
-                                built.push(value)
+                                built.push(value)           // Pushing function call to built line
                             } else if (func_args[func]) {
                                 if (func_args[func].includes(value)) {
-                                    built.push(value)
+                                    built.push(value)       // Pushing function argument to built line
                                 }
                             }
                         }
                         break
                     }
 
-                    case 'SPACE': {                        // Refer to spaces
+                    case 'SPACE': {                         // Refer to spaces
                         if (context === 'ARGUMENTS') {
                             if (lexered[parseInt(lexer_item) + 1].token === 'WORD' && lexered[parseInt(lexer_item) - 1].token === 'WORD') {
                                 built.push(', ')
@@ -79,7 +81,7 @@ module.exports = class {
                         }
                         break
                     }
-                    case 'SIGNS': case 'AND': case 'NOT': {
+                    case 'SIGNS': case 'AND': case 'NOT': {  // Refer to signs, not and and keywords
                         if (context === 'ARGUMENTS') {
                             built.push(')')
                             context = undefined
