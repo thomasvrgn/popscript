46f06215a2e461ef4ea5219f23bb48ee896bdbe9
NessMC <contact@nessmc.fr>
2020-06-19 12:34:54 +0200

    🔨 Property token updated.

--- src/core/tokens/tokens.ts
+++ src/core/tokens/tokens.ts
@@ -14,7 +14,7 @@ export default {
     L_PAREN     : /\(/                , // Match any left parenthesis
     R_PAREN     : /\)/                , // Match any right parenthesis
     ARGUMENTS   : /=>/                , // Match arguments start like fn test => coucou
-    PROPERTY    : /:/                 , // Match property
+    PROPERTY    : /:\w+/              , // Match property
     INDEX       : /<.*?>/             , // Match index
 
     // TYPES
