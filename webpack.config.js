017b9c6ee1b45eb9228fa5e9fc1bc7b0288278a0
NessMC <contact@nessmc.fr>
2020-06-20 21:01:36 +0200

    🚚 Build file moved from build folder to dist folder.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './dist/index.js',
   target: 'node',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist', 'build'),
     filename: 'popscript.js'
   }
 }
