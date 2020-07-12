398c6816bcee50d2e9cb8f0a254b493042ee9bab
NessMC <contact@nessmc.fr>
2020-07-12 09:38:11 +0200

    ✨ Tabs addon added.

---
+++ dist/core/addons/tabs.js
@@ -0,0 +1,21 @@
+"use strict";
+/*//////////////////////////////////
+         POPSCRIPT LANGUAGE
+                Addon
+//////////////////////////////////*/
+exports.__esModule = true;
+var Word = /** @class */ (function () {
+    function Word() {
+    }
+    Word.prototype.exec = function (token, value, context, specs, tokens, index) {
+        if (token === void 0) { token = ''; }
+        if (value === void 0) { value = ''; }
+        if (context === void 0) { context = []; }
+        if (tokens === void 0) { tokens = []; }
+        if (index === void 0) { index = 0; }
+        return value;
+        return;
+    };
+    return Word;
+}());
+exports["default"] = Word;
