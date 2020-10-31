832a613a3a27a3f58ac0f2e072ea9b7b06679445
thomasvergne <thomas@quark-lang.dev>
2020-10-31 13:09:37 +0100

    🧼 Tabdown cleaned up.

--- src/core/tabdown.ts
+++ src/core/tabdown.ts
@@ -6,51 +6,62 @@ export default class Tabdown {
   private tabsize: any;
 
   constructor(content) {
-    this.content = content.filter(x => x !== '')
+    this.content = content.filter(x => x !== '');
     this.AST = {
       root: {}
-    }
-    this.parents = []
-    this.code = []
+    };
+    this.parents = [];
+    this.code = [];
   }
 
   private writeObject(array: Array<string> = [], property: string = '', value: any) {
-    let item = this.AST['root']
+    let item = this.AST['root'];
     if (array.length > 0) {
       for (const itemt in array) {
         if (array.hasOwnProperty(itemt)) {
-          if (parseInt(itemt) + 1 === array.length) item[array[itemt]][property] = value
-          item = item[array[itemt]]
+          if (parseInt(itemt) + 1 === array.length) item[array[itemt]][property] = value;
+          item = item[array[itemt]];
         }
       }
     } else {
-      item[property] = value
+      item[property] = value;
     }
   }
 
   private buildAST(index: number = 0) {
 
-    const line = this.content[index],
-      ft_line = this.content[index + 1]
+    const line = this.content[index];
+    const ft_line = this.content[index + 1];
     if (line.match(/^\s+/)) {
       if (!this.tabsize) {
-        this.tabsize = line.match(/^\s+/)[0].length
+        this.tabsize = line.match(/^\s+/)[0].length;
       }
     }
-    const depth = line.match(/^\s+/) ? line.match(/^\s+/)[0].length / this.tabsize : 0,
-      ft_depth = ft_line ? this.content[index + 1].match(/^\s+/) ? this.content[index + 1].match(/^\s+/)[0].length / this.tabsize : 0 : undefined
+    const depth: number = line
+      .match(/^\s+/) 
+      ? line
+          .match(/^\s+/)[0]
+          .length / this.tabsize 
+      : 0;
+    const ft_depth: number | undefined = ft_line 
+        ? this.content[index + 1]
+            .match(/^\s+/) 
+          ? this.content[index + 1]
+              .match(/^\s+/)[0].length / this.tabsize 
+          : 0 
+        : undefined;
 
     if (line.trimRight().endsWith(':')) {
-      this.writeObject(this.parents, line.trim() + '||' + index + '||' + depth, {})
+      this.writeObject(this.parents, line.trim() + '||' + index + '||' + depth, {});
       this.parents.push(line.trim() + '||' + index + '||' + depth)
     } else {
-      this.writeObject(this.parents, line.trim() + '||' + index + '||' + depth, '')
+      this.writeObject(this.parents, line.trim() + '||' + index + '||' + depth, '');
     }
     if (ft_depth < depth) {
-      this.parents = this.parents.slice(0, ft_depth)
+      this.parents = this.parents.slice(0, ft_depth);
     }
     if (ft_line) {
-      this.buildAST(index + 1)
+      this.buildAST(index + 1);
     }
 
   }
@@ -60,22 +71,64 @@ export default class Tabdown {
     for (const child in item) {
       if (item.hasOwnProperty(child)) {
         if (typeof item[child] === 'object') {
-          this.code.push(new Array(parseInt(child.split('||')[2])).fill(new Array(this.tabsize).fill(' ').join('')).join('') + child.split('||')[0].slice(0, child.split('||')[0].length - 1) + '{')
-          this.addBrackets(item[child])
-          this.code.push(new Array(parseInt(child.split('||')[2])).fill(new Array(this.tabsize).fill(' ').join('')).join('') + '}')
+          this.code
+            .push(
+              new Array(
+                parseInt(
+                  child.split('||')[2]
+                )
+              )
+            .fill(
+              new Array(
+                this.tabsize
+              )
+              .fill(' ')
+              .join('')
+            )
+            .join('') + child.split('||')[0].slice(0, child.split('||')[0].length - 1) + '{');
+          this.addBrackets(item[child]);
+          this.code
+            .push(
+              new Array(
+                parseInt(
+                  child.split('||')[2]
+                )
+              )
+              .fill(
+                new Array(
+                  this.tabsize
+                )
+                .fill(' ')
+                .join('')
+              ).join('') + '}'
+            );
         } else {
-          this.code.push(new Array(parseInt(child.split('||')[2])).fill(new Array(this.tabsize).fill(' ').join('')).join('') + child.split('||')[0])
-          this.addBrackets(item[child])
+          this.code
+            .push(
+              new Array(
+                parseInt(
+                  child.split('||')[2]
+                )
+              )
+              .fill(
+                new Array(
+                  this.tabsize
+                )
+                .fill(' ')
+                .join(''))
+                .join('') + child.split('||')[0]
+            );
+          this.addBrackets(item[child]);
         }
       }
     }
 
   }
 
-  public tab() {
-    this.buildAST(0)
-    this.addBrackets(this.AST.root)
-    return this.code
+  public tab(): string[] {
+    this.buildAST(0);
+    this.addBrackets(this.AST.root);
+    return this.code;
   }
 
 }
